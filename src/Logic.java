46bc8db91449f50135b2e66155dff9a9d31a344a
JasonSaber <jsaber19@windwardschool.org>
2018-10-18 10:53:20 -0700

    starting bruteforce

--- src/Logic.java
+++ src/Logic.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Logic {
+    private String password;
+    public int count = 0;
+    private int method;
+    private char[] characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()".toCharArray(); // https://stackoverflow.com/questions/17575840/better-way-to-generate-array-of-all-letters-in-the-alphabet
+
+    public Logic(){
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Type in a password you want to break");
+        password = kb.nextLine();
+        System.out.println("Type '1' if you want to break via brute force. Type '2' if you want to break via common words. Type '3' if you want to break by ");
+        method = kb.nextInt(); // temp until we set up button events
+    }
+
+    public void bruteForce(){
+        for (int i = 0; i < password.length(); i++){
+            for (int j = 0; j < characters.length; j++){
+                count++;
+                if (password.charAt(i) == characters[j]){  }
+            }
+        }
+    }
 }
