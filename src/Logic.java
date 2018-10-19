1e2d13cfb616f018d215c547003c022ffa6cdf88
JasonSaber <jsaber19@windwardschool.org>
2018-10-19 15:03:21 -0700

    need to rethink bruteforce with recursion; common words

--- src/Logic.java
+++ src/Logic.java
@@ -10,16 +10,51 @@ public class Logic {
         Scanner kb = new Scanner(System.in);
         System.out.println("Type in a password you want to break");
         password = kb.nextLine();
-        System.out.println("Type '1' if you want to break via brute force. Type '2' if you want to break via common words. Type '3' if you want to break by ");
+        System.out.println("Type '1' if you want to break via brute force. Type '2' if you want to break via common passwords. Type '3' if you want to break by ");
         method = kb.nextInt(); // temp until we set up button events
     }
 
-    public void bruteForce(){
-        for (int i = 0; i < password.length(); i++){
-            for (int j = 0; j < characters.length; j++){
-                count++;
-                if (password.charAt(i) == characters[j]){  }
+    public int bruteForce(){
+        int length = 1; // current length testing
+
+//        int check = 0;
+//        int tracker = 0; // counts the number of attempts made
+//        int level = 1; // the length of the password we're guessing
+//        String saved = "";
+//        while (check == 0){ // until we find the password
+//            String guess = "";
+//                int k = 0; // arbitrary int to track level
+//                while (k < level){
+//                    guess = "";
+//                    for (int i = 0; i < characters.length; i++){
+//                        guess+=characters[i];
+//                        tracker++;
+//                        if (guess.equals(password)) {
+//                            check++;
+//                            saved = guess;
+//                        }
+//                        else { guess = guess.substring(0, guess.length()-1); }
+//                    }
+//
+//                }
+//
+//            tracker++;
+//        }
+        return 0;
+    }
+
+    public int commonPasswords(){
+        int tracker = 0;
+        String[] common = {"123456", "password", "12345678", "qwerty", "12345",
+                "123456789", "letmein", "1234567", "football", "iloveyou", "admin",
+                "welcome", "monkey", "login", "abc123", "starwars", "123123", "dragon",
+                "passw0rd", "master", "hello", "freedom", "whatever", "qazwsx", "trustno1"};
+        for (String s : common){
+            tracker++;
+            if (s.equals(password)){
+                return 1;
             }
         }
+        return -1;
     }
 }
