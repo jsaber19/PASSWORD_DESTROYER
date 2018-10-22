4f31abf80034c90337a79472865e070baf24e53f
JasonSaber <jsaber19@windwardschool.org>
2018-10-22 12:30:10 -0700

    tentative brute force algorithm; will need to test and debug later

--- src/Logic.java
+++ src/Logic.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Logic {
@@ -14,33 +15,64 @@ public class Logic {
         method = kb.nextInt(); // temp until we set up button events
     }
 
+    // http://prembharticodes.blogspot.com/2011/10/brute-force-algorithm-for-password.html
     public int bruteForce(){
-        int length = 1; // current length testing
 
-//        int check = 0;
-//        int tracker = 0; // counts the number of attempts made
-//        int level = 1; // the length of the password we're guessing
-//        String saved = "";
-//        while (check == 0){ // until we find the password
-//            String guess = "";
-//                int k = 0; // arbitrary int to track level
-//                while (k < level){
-//                    guess = "";
-//                    for (int i = 0; i < characters.length; i++){
-//                        guess+=characters[i];
-//                        tracker++;
-//                        if (guess.equals(password)) {
-//                            check++;
-//                            saved = guess;
-//                        }
-//                        else { guess = guess.substring(0, guess.length()-1); }
-//                    }
-//
-//                }
-//
-//            tracker++;
-//        }
-        return 0;
+        // initialize char array for guesses
+        char[] guess = new char[0];
+        Arrays.fill(guess, characters[0]);
+
+        // make attempt to represent guess as a String
+        String attempt = "";
+        for (char c : guess) { attempt += c; }
+
+        while(count < 5000){ // until you've reached 5000 attempts
+            if (attempt.equals(password)){
+                count++; // final increment
+                return 1; // found it!
+            }
+
+            // set attempt to represent the current guess
+            attempt = "";
+            for (char c : guess) { attempt += c; }
+            count++; // means the if branch wasn't activated but still need to track each attempt
+
+
+            /**
+             * Making new passwords
+             */
+            int index = guess.length - 1; // always start index from last index of current guess
+
+            while (index >= 0){ // will make a new guess
+
+                // if you've gone through every char in characters for the current index
+                if (guess[index] == characters[characters.length-1]) {
+
+                    // if the first element has also gone through every available char in characters
+                    if (index == 0){
+                        guess = new char[guess.length + 1]; // make a new guess that's one element longer
+                        Arrays.fill(guess, characters[0]); // default every element the first element of characters
+                        break; // try the password
+                    }
+
+                    // if you have not reached the first element yet
+                    else {
+                        guess[index] = characters[0]; // default the element back to the first element of characters
+                        index--; // move on to the index one lower
+                    }
+                }
+
+                // if you haven't gone through every available char for the current index
+                else {
+                    guess[index] = characters[Arrays.binarySearch(characters, guess[index]) + 1]; // try the next char at the current index
+                    break; // try the password
+                }
+
+            } // end while loop for making passwords
+
+        } // end while loop for guessing passwords
+
+        return -1; // couldn't find it
     }
 
     public int commonPasswords(){
